7ef84da86788f6ce77690772827f7f586d48d9d0
Duong <buidangduong08112006@gmail.com>
2026-09-24 21:50:41 +0700

    Create SQLAlchemy models for all tables

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.user import User
+from app.models.folder import Folder
+from app.models.document import Document
+from app.models.job import Job
+from app.models.job_file import JobFile
+from app.models.citation import Citation
+
+__all__ = [
+    "User",
+    "Folder",
+    "Document",
+    "Job",
+    "JobFile",
+    "Citation",
+]
